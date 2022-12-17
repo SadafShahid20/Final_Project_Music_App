a35287c64607568cf8c5d85f32df3a54dba35aaa
Talha2647 <107463181+SadafShahid20@users.noreply.github.com>
2022-12-17 09:09:34 +0100

    changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://localhost:4000/";
-const baseURL = "http://localhost:4000/";
+const baseURL = '/';
 
 
 export const validateUser = async (token) => {
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/user',
+    '/api',
     createProxyMiddleware({
       target: 'http://localhost:4000',
       changeOrigin: true,
